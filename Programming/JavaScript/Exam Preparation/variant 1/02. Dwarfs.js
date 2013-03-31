745c9f59479b0d4b2f6ac9e7a3bd295dd1037c10
nikolay.marinov <niki.funky@gmail.com>
2013-03-31 18:17:43 +0300

    JS prep

---
+++ Programming/JavaScript/Exam Preparation/variant 1/02. Dwarfs.js
@@ -0,0 +1,46 @@
+﻿function Solve(args) {
+    var maxSum = -100000;
+
+    var valley = [];
+    var input = args[0].split(',');
+    for (var i = 0; i < input.length; i++) {
+        valley[i] = +input[i];
+    }
+
+    var numberOfPatterns = parseInt(args[1]);
+
+    for (var i = 0; i < numberOfPatterns; i++) {
+        var inputPattern = args[2 + i].split(',');
+        var pattern = [];
+        for (var n = 0; n < inputPattern.length; n++) {
+            pattern[n] = +inputPattern[n];
+        }
+
+        var visitedIdx = {};
+        var patSum = 0;
+        var valleyIndex = 0;
+        var patternIndex = 0;
+
+        while (valleyIndex >= 0 && valleyIndex < valley.length) {
+            patSum += valley[valleyIndex];
+            visitedIdx[valleyIndex] = true;
+
+            valleyIndex += pattern[patternIndex];
+            if (visitedIdx[valleyIndex] == true) {
+                break;
+            }
+
+            if (patternIndex < pattern.length - 1) {
+                patternIndex++;
+            }
+            else {
+                patternIndex = 0;
+            }
+        }
+        if (patSum > maxSum) {
+            maxSum = patSum;
+        }
+    }
+    return maxSum;
+}
+
---
+++ Programming/JavaScript/Exam Preparation/variant 1/03. Console.js
@@ -0,0 +1,66 @@
+﻿function Solve(args) {
+    var words = [];
+    var usedWords = 0;
+    var resultText = [];
+    var numberOfLines = parseInt(args[0]);
+    var numberOfLetters = parseInt(args[1]);
+
+    for (var i = 0; i < numberOfLines; i++) {
+        var inputWords = args[2 + i].split(' ');
+
+        words.push.apply(words, inputWords);
+    }
+    //remove empty spaces
+    words = words.filter(function (val) {
+        return !(val === "" || typeof val == "undefined" || val === null);
+    });
+
+    while (usedWords < words.length) {
+        var lineLetters = 0;
+        var lineWords = 0;
+        var lineFirstWordIdx = usedWords;
+
+        lineWords++;
+        usedWords++;
+        lineLetters += words[lineFirstWordIdx].length;
+
+        for (var wordToUseIdx = lineFirstWordIdx + 1; wordToUseIdx < words.length; wordToUseIdx++) {
+            var potentialLinelength = words[wordToUseIdx].length + lineLetters + lineWords;
+            if (potentialLinelength < numberOfLetters) {
+                lineWords++;
+                usedWords++;
+                lineLetters += words[wordToUseIdx].length;
+            }
+            else {
+                break;
+            }
+        }
+        var wordSeparations = lineWords - 1;
+
+        if (wordSeparations != 0) {
+            var excessSpaces = numberOfLetters - lineLetters;
+            var minSpacesPerSeparation = Math.floor(excessSpaces / wordSeparations);
+            var lineLastWordInd = lineFirstWordIdx + lineWords - 1;
+            var lastAdditionallySpacedWordInd = lineFirstWordIdx + (Math.floor(excessSpaces % wordSeparations));
+            var minSpacing = (new Array(minSpacesPerSeparation)).join(" ");
+
+            resultText = resultText + words[lineFirstWordIdx];
+
+            for (var i = lineFirstWordIdx + 1; i <= lineLastWordInd; i++) {
+                if (i <= lastAdditionallySpacedWordInd) {
+                    resultText = resultText + ' ';
+                }
+
+                resultText = resultText + minSpacing;
+                resultText = resultText + words[i];
+            }
+
+            resultText = resultText + "\n";
+        }
+        else {
+            resultText = resultText + "\n" + words[lineFirstWordIdx];
+        }
+    }
+    return resultText;
+}
+
