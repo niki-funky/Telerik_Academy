524d1d94d870aa73f81d53d5f0a73622b21a2ff0
nikolay.marinov <niki.funky@gmail.com>
2013-04-26 20:29:09 +0300

    JS2

--- Programming/JavaScript 2/02. DOM-manipulation/scripts/textarea.creator.js
+++ Programming/JavaScript 2/02. DOM-manipulation/scripts/textarea.creator.js
@@ -1,1 +1,13 @@
-﻿
+﻿var newTextArea = document.getElementsByTagName('textarea')[0];
+
+function changeFontColor() {
+	var fontColor = document.getElementById('fontColor').value;
+
+	newTextArea.style.color = fontColor;
+}
+
+function changeBackgroundColor() {
+	var background = document.getElementById('backgroundColor').value;
+
+	newTextArea.style.backgroundColor = background;
+}
