6a59c9cd36c81e0ff854b19f9f4bbb2655ea110d
nikolay.marinov <niki.funky@gmail.com>
2013-11-24 16:12:29 +0200

    js 1

--- Programming/JavaScript/EXAM/03. exercise.js
+++ Programming/JavaScript/EXAM/03. exercise.js
@@ -1,32 +1,83 @@
-﻿function Solve(args) {
+﻿var Solve = (function () {
+    'use strict';
 
-    var i, k, l;
-    var arr = {};
-    var line = "";
-    var words = [];
-    var funcName = "";
+    var vars = {};
 
-
-
-    for (i = 0; i < args.length; i++) {
-
-        line = args[i].replace(/\s{2,}/g, ' ');
-        var re = new RegExp(/\[([^()]+)\]/g);
-        var brackets = re.exec(line);
-        var bracks = brackets[0].replace(/\s/g, '');
-        line = line.replace(new RegExp(/\[([^()]+)\]/g), bracks);
-        //line = line.replace(/\s{2,}/g, ' ');
-        words = line.split(' ');
-        //funcName = line.exec(new RegExp(/\b.*\b/, 'g');
-
-        for (k = 0; k < args[i].length; k++) {
-            if (line[k] = 'd') {
-
+    var commands =
+        {
+            sum: function (arg) {
+                return arg.reduce(function (a, b) {
+                    return a + b;
+                })
             }
+
+            , min: function (arg) {
+                return Math.min.apply(Math, arg);
+            }
+
+            , max: function (arg) {
+                return Math.max.apply(Math, arg);
+            }
+
+            , avg: function (arg) {
+                return parseInt(commands.sum(arg) / arg.length, 10);
+            }
+
+            , assign: function (arg) {
+                return arg;
+            }
+        }
+
+    function addRange(arr, els) {
+        els.forEach(function (el) {
+            arr.push(el)
+        })
+    }
+
+    function executeCommand(name, cmd, list) {
+
+        var result = []
+
+        list.forEach(function (el) {
+            if (!isNaN(+el)) result.push(+el)
+
+            else if (el in vars) addRange(result, vars[el])
+
+            else throw new Error(el)
+        })
+
+        vars[name] = cmd !== 'assign' && [commands[cmd](result)] || result
+    }
+
+    function parseCommand(command) {
+
+        if (!command.match(/^\s*def/))
+            command = 'def __result__ ' + command
+
+        try {
+            var match = command.match(/^\s*def\s*(\S*)\s*(\S*)\s*\[\s*(.+?)\s*\]/)
+
+            if (match[2] != "" &&
+                match[2] != "sum" &&
+                match[2] != "min" &&
+                match[2] != "max" &&
+                match[2] != "avg") {
+                match[1] = match[2];
+                match[2] = '';
+            }
+            executeCommand(match[1], match[2] || 'assign', match[3].split(/\s*,\s*/))
+        }
+
+        catch (e) {
+            console.error(e.message)
         }
     }
 
+    return function (args) {
+        while (args.length) {
+            parseCommand(args.shift())
 
-
-    return 0;
-}
+            if (vars['__result__']) return vars['__result__'][0]
+        }
+    }
+}());
